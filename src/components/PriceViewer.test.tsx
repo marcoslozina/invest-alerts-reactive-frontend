7e34f9f49ef7fd8290956a37dd8cd830c5844e95
Marcos Lozina <marcoslozina@gmail.com>
2025-08-05 22:36:33 -0300

    fix: fix coverage

--- src/components/PriceViewer.test.tsx
+++ src/components/PriceViewer.test.tsx
@@ -1,11 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import PriceViewer from './PriceViewer';
 import { describe, it, expect } from 'vitest';
+import '../test/i18nForTests'; // ✅ CORRECTO si tu estructura es src/components y src/test
 
 describe('PriceViewer', () => {
   it('renders without crashing', () => {
     render(<PriceViewer />);
-    const status = screen.getByText(/cargando/i); // si tiene algún texto visible como "Cargando..."
+    const status = screen.getByText('Cargando...');
     expect(status).toBeInTheDocument();
   });
 });
